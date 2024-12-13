61138e992ed03bdb660460a3c2c7abd139956828
shaw <1939484341@qq.com>
2024-12-13 10:54:17 +0800

    vue学习资料

---
+++ my-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app">
+        <Person/>
+    </div>
+</template>
+
+<script>
+
+import Person from './components/person.vue';
+import MyComputed from './components/MyComputed.vue'
+
+export default{
+    name:'App',
+    components:{Person}
+}
+</script>
+
+<style scoped>
+.app{
+    background-color: #ddd;
+    box-shadow: 0 0 10px; 
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
---
+++ my-vue/src/components/MyComputed.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="mycomputed">
+    姓：<input type="text" v-model="firstName"> <br>
+    名：<input type="text" v-model="lastName"> <br>
+    全名：<span>{{ fullName }}</span> <br>
+    <button @click="changeName">改成li-si</button>
+ </div>
+</template>
+
+<script setup name="MyComputed">
+    import { ref, computed } from "vue";
+    let firstName = ref('zhang')
+    let lastName = ref('san')
+
+    let fullName = computed({
+        get(){
+            console.log('set')
+            return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+        },
+        set(va1){
+            const [x1, x2] = va1.split('-') 
+            console.log('get', x1, x2)
+            firstName.value = x1
+            lastName.value = x2
+        }
+    })
+
+    function changeName(){
+        fullName.value = 'li-si'
+    }
+
+</script>
+
+<style scoped>
+
+</style>
